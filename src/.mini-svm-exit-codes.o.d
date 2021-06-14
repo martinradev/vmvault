e042bd8a74c6fd5d6a8a4d51496f54389939afbe
Martin Radev <martin.b.radev@gmail.com>
2021-06-14 23:18:23 +0300

    Remove .d file

--- src/.mini-svm-exit-codes.o.d
+++
@@ -1,6 +0,0 @@
-mini-svm-exit-codes.o: \
- /home/sisu/code/linux-mini-svm-hv/src/mini-svm-exit-codes.c \
- include/linux/kconfig.h include/generated/autoconf.h \
- include/linux/compiler_types.h include/linux/compiler_attributes.h \
- include/linux/compiler-gcc.h \
- /home/sisu/code/linux-mini-svm-hv/src/mini-svm-exit-codes.h
