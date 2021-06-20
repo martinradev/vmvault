98c302d2a834bf353f08d37f5636a010a1ea1ee9
Martin Radev <martin.b.radev@gmail.com>
2021-06-20 10:21:44 +0300

    Remove rendudant code in util.c

--- vm-code/util.c
+++ vm-code/util.c
@@ -1,10 +1,2 @@
 #include "util.h"
 
-void measure_random_access_linked_list(const unsigned long *start, unsigned long *out) {
-	asm volatile(
-		""
-		: /* out */
-		: /* in */
-		: /* clobbers */
-	)
-}
