0180a1302d7d0cecf0232898ca8977450162e09e
Martin Radev <martin.b.radev@gmail.com>
2021-06-20 10:21:24 +0300

    Add vmmcall overloads, rdtsc with barriers

--- vm-code/util.h
+++ vm-code/util.h
@@ -1,6 +1,36 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+static inline unsigned long rdtsc_and_bar(void) {
+	unsigned long tsc;
+	asm volatile(
+		"rdtsc\n\t"
+		"mfence\n\t"
+		"shl $32, %%rdx\n\t"
+		"or %%rdx, %%rax\n\t"
+		"mov %%rax, %0\n\t"
+		: "=r"(tsc)
+		:
+		: "%rax", "%rdx"
+	);
+	return tsc;
+}
+
+static inline unsigned long bar_and_rdtsc(void) {
+	unsigned long tsc;
+	asm volatile(
+		"mfence\n\t"
+		"rdtsc\n\t"
+		"shl $32, %%rdx\n\t"
+		"or %%rdx, %%rax\n\t"
+		"mov %%rax, %0\n\t"
+		: "=r"(tsc)
+		:
+		: "%rax", "%rdx"
+	);
+	return tsc;
+}
+
 static inline unsigned long rdtsc(void) {
 	unsigned long tsc;
 	asm volatile(
@@ -44,14 +74,33 @@ static inline unsigned long rd_aperf(void) {
 	return clock;
 }
 
-static inline void vmmcall(unsigned long rax) {
+static inline void vmmcall(
+		unsigned long cmd,
+		unsigned long arg1,
+		unsigned long arg2,
+		unsigned long arg3) {
 	asm volatile(
 		"movq %0, %%rax\n\t"
+		"movq %1, %%rdi\n\t"
+		"movq %2, %%rsi\n\t"
+		"movq %3, %%rdx\n\t"
 		"vmmcall\n\t"
 		:
-		: "r"(rax)
-		: "%rax", "%rbx", "%rcx", "%rdx"
+		: "r"(cmd), "r"(arg1), "r"(arg2), "r"(arg3)
+		: "%rax", "%rdi", "%rsi", "%rdx"
 	);
+}
+
+static inline void vmmcall(unsigned long cmd) {
+	vmmcall(cmd, 0, 0, 0);
+}
+
+static inline void vmmcall(unsigned long cmd, unsigned long arg1) {
+	vmmcall(cmd, arg1, 0, 0);
+}
+
+static inline void vmmcall(unsigned long cmd, unsigned long arg1, unsigned long arg2) {
+	vmmcall(cmd, arg1, arg2, 0);
 }
 
 #define MEASURE(OPS) \
