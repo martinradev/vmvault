24c52848f16e8df9caad37b18398f57040d38087
Martin Radev <martin.b.radev@gmail.com>
2021-06-26 15:36:53 +0300

    hv: Clean-up, enable sse, register key, do enc

--- hv-user-space/hv-user-space-program.cpp
+++ hv-user-space/hv-user-space-program.cpp
@@ -9,6 +9,7 @@
 #include <unistd.h>
 #include <vector>
 #include <string>
+#include <array>
 
 #include <sys/ioctl.h>
 #include <sys/mman.h>
@@ -18,12 +19,15 @@
 #include "mini-svm-exit-codes.h"
 #include "mini-svm-common-structures.h"
 #include "mini-svm-vmcb.h"
+#include "mini-svm-communication-block.h"
 
 #include "hv-util.h"
 #include "hv-microbench-structures.h"
 
+#include "phys_addr_util.h"
+
 #define MINI_SVM_MAX_PHYS_SIZE (64UL * 1024UL * 1024UL)
-#define IMAGE_START 0x18000UL
+#define IMAGE_START 0x8000UL
 
 #define EFER_SVME (1UL << 12U)
 #define EFER_LME (1UL << 8U)
@@ -35,53 +39,21 @@
 #define CR0_PG (1UL << 31U)
 #define CR4_PAE (1UL << 5U)
 #define CR4_PGE (1UL << 7U)
+#define CR4_OSFXSR (1UL << 9U)
+#define CR4_OSXMMEXCPT (1UL << 10U)
 
 // The start of guest physical memory is for the GPT which currently just takes two physical pages
 // Writes to memory at an address lower than this one should be forbidden when they go via write_virt_memory.
 #define PHYS_BASE_OFFSET 0x3000U
 
-enum class MicrobenchExperiment {
-	ICacheSize,
-	DCacheSize,
-	DTLBSize,
-	ITLBSize,
-
-	Unknown
-};
-
 static void *guest_memory = NULL;
-MicrobenchExperiment experimentType;
-static FILE *data_access_results_file;
-static FILE *page_access_results_file;
-static FILE *instruction_fetch_results_file;
-static FILE *instruction_fetch_page_results_file;
-
-
-static void report_experiment_result(unsigned long size, unsigned long ncycles) {
-	switch(experimentType) {
-		case MicrobenchExperiment::ICacheSize:
-			fprintf(instruction_fetch_results_file, "%.16lu bytes: %.16lu cycles\n", size * 64UL, ncycles);
-			break;
-		case MicrobenchExperiment::DCacheSize:
-			fprintf(data_access_results_file, "%.16lu bytes: %.16lu cycles\n", size * 64UL, ncycles);
-			break;
-		case MicrobenchExperiment::DTLBSize:
-			fprintf(page_access_results_file, "%.16lu pages: %.16lu cycles\n", size, ncycles);
-			break;
-		case MicrobenchExperiment::ITLBSize:
-			fprintf(instruction_fetch_page_results_file, "%.16lu pages: %.16lu cycles\n", size, ncycles);
-			break;
-		default:
-			fprintf(stderr, "Unknown option\n");
-			exit(1);
-	}
-}
 
 int mini_svm_mm_write_phys_memory(void *phys_base, __u64 phys_address, void *bytes, __u64 num_bytes) {
 	if (phys_address + num_bytes > MINI_SVM_MAX_PHYS_SIZE) {
 		return false;
 	}
 
+	printf("write phys: %lx\n", phys_address);
 	memcpy((unsigned char *)phys_base + phys_address, bytes, num_bytes);
 
 	return true;
@@ -94,6 +66,10 @@ bool mini_svm_mm_write_virt_memory(void *phys_base, __u64 virt_address, void *by
 	return mini_svm_mm_write_phys_memory(phys_base, virt_address, bytes, num_bytes);
 }
 
+size_t get_phys_memory_size() {
+	return (size_t)sysconf(_SC_PHYS_PAGES) * (size_t)sysconf(_SC_PAGE_SIZE);
+}
+
 int mini_svm_construct_1gb_gpt(void *phys_base) {
 	// We just need 2 pages for the page table, which will start at physical address 0 and will have length of 1gig.
 	const __u64 pml4e = mini_svm_create_entry(0x1000, MINI_SVM_PRESENT_MASK | MINI_SVM_USER_MASK | MINI_SVM_WRITEABLE_MASK);
@@ -103,6 +79,15 @@ int mini_svm_construct_1gb_gpt(void *phys_base) {
 	}
 	if (!mini_svm_mm_write_phys_memory(phys_base, 0x1000, (void *)&pdpe, sizeof(pdpe))) {
 		return false;
+	}
+
+	const size_t one_gig { 1024UL * 1024UL * 1024UL };
+	const size_t num_one_gig_pages { (get_phys_memory_size() + one_gig - 1UL) / one_gig };
+	for (size_t i = 0; i < num_one_gig_pages; ++i) {
+		const __u64 pdpe = mini_svm_create_entry(one_gig * (i + 1UL), MINI_SVM_PRESENT_MASK | MINI_SVM_USER_MASK | MINI_SVM_WRITEABLE_MASK | MINI_SVM_LEAF_MASK);
+		if (!mini_svm_mm_write_phys_memory(phys_base, 0x1000UL + 0x8UL * (i + 1UL), (void *)&pdpe, sizeof(pdpe))) {
+			return false;
+		}
 	}
 	return true;
 }
@@ -146,8 +131,6 @@ static void setup_ctrl(struct mini_svm_vmcb_control *ctrl) {
 	ctrl->vec4.vmrun_intercept = 1;
 	ctrl->vec4.vmmcall_intercept = 1;
 	ctrl->tlb_control = 1;
-	//ctrl->vec3.rdtsc_intercept = 1;
-	//ctrl->vec4.rdtscp_intercept = 1;
 }
 
 static void setup_save(struct mini_svm_vmcb_save_area *save) {
@@ -155,7 +138,7 @@ static void setup_save(struct mini_svm_vmcb_save_area *save) {
 	save->efer = EFER_SVME | EFER_LME | EFER_LMA;
 	save->cr0 = (CR0_PE | CR0_PG);
 	save->cr3 = 0x0;
-	save->cr4 = (CR4_PAE | CR4_PGE);
+	save->cr4 = (CR4_PAE | CR4_PGE | CR4_OSXMMEXCPT | CR4_OSFXSR);
 
 	// Setup gdt
 	save->reg_gdtr.base = 0x0;
@@ -232,118 +215,20 @@ int mini_svm_intercept_cpuid(struct mini_svm_vm_state *state) {
 }
 
 int mini_svm_intercept_vmmcall(struct mini_svm_vm_state *state) {
-	const VmmCall cmd = (const VmmCall)state->regs.rax;
+	const unsigned long cmd = state->regs.rax;
 	const unsigned long arg1 = state->regs.rdi;
 	const unsigned long arg2 = state->regs.rsi;
 	const unsigned long arg3 = state->regs.rdx;
-
-	const unsigned long cache_line_size = 64UL;
-	const unsigned long page_size = 0x1000UL;
-
-	switch(cmd) {
-		case VmmCall::RequestRandomPageAccessSeq:
-		case VmmCall::RequestRandomDataAccessSeq:
-		{
-			const size_t granularity_size = (cmd == VmmCall::RequestRandomDataAccessSeq) ? cache_line_size : page_size;
-			const unsigned long start_phys_addr = arg1;
-			const unsigned long start_rand_va = arg2;
-			const unsigned long num_elements = arg3;
-			std::vector<unsigned long> seq, seq2;
-			generate_random_unique_sequence(num_elements, seq);
-			generate_random_unique_sequence(granularity_size / cache_line_size, seq2);
-			unsigned long prev_index = 0;
-			for (unsigned long i = 0; i < num_elements; ++i) {
-				unsigned long next_index = seq[i] * granularity_size;
-				if (cmd == VmmCall::RequestRandomPageAccessSeq && i != num_elements - 1) {
-					// Additionally, we have to access a random cache line within the page to ensure that we do not thrash
-					// the cache but the TLB instead.
-					next_index += cache_line_size * seq2[i % seq2.size()];
-				}
-				const unsigned long next_va = next_index + start_rand_va;
-				if (!mini_svm_mm_write_virt_memory(guest_memory, start_phys_addr + prev_index, (void *)&next_va, sizeof(next_va))) {
-					printf("Failed to write index\n");
-					return -1;
-				}
-				prev_index = next_index;
-			}
-			break;
-		}
-		case VmmCall::RequestRandomJmpAccessSeq:
-		case VmmCall::RequestRandomJmpPageSeq:
-		{
-			const size_t granularity_size = (cmd == VmmCall::RequestRandomJmpAccessSeq) ? cache_line_size : page_size;
-			const unsigned long start_phys_addr = arg1;
-			const unsigned long start_rand_va = arg2;
-			const unsigned long num_elements = arg3;
-			std::vector<unsigned long> seq, seq2;
-			generate_random_unique_sequence(num_elements, seq);
-			generate_random_unique_sequence(granularity_size / cache_line_size, seq2);
-			unsigned long prev_index = 0;
-			for (unsigned long i = 0; i < num_elements - 1; ++i) {
-				unsigned long next_index = seq[i] * granularity_size;
-				if (cmd == VmmCall::RequestRandomJmpPageSeq && i != num_elements - 2) {
-					// Additionally, we have to access a random cache line within the page to ensure that we do not thrash
-					// the cache but the TLB instead.
-					next_index += cache_line_size * seq2[i % seq2.size()];
-				}
-				unsigned next_gpa = next_index + start_phys_addr;
-				unsigned next_gva = next_index + start_rand_va;
-				unsigned prev_gpa = prev_index + start_phys_addr;
-				unsigned prev_gva = prev_index + start_rand_va;
-				unsigned rel_addr = next_gva - (prev_gva + 0x5UL);
-				unsigned char jmp_rel_machine_code[5];
-				jmp_rel_machine_code[0] = 0xE9UL;
-				memcpy(&jmp_rel_machine_code[1], &rel_addr, sizeof(rel_addr));
-				if (!mini_svm_mm_write_virt_memory(guest_memory, prev_gpa, jmp_rel_machine_code, sizeof(jmp_rel_machine_code))) {
-					printf("Failed to write index\n");
-					return -1;
-				}
-				prev_index = next_index;
-			}
-			unsigned prev_gpa = start_phys_addr + prev_index;
-			const unsigned char jmp_rbx[2] = {0xffU, 0xe3U};
-			if (!mini_svm_mm_write_virt_memory(guest_memory, prev_gpa, (void *)jmp_rbx, sizeof(jmp_rbx))) {
-				printf("Failed to write index\n");
-				return -1;
-			}
-			break;
-		}
-		case VmmCall::ReportResult:
-		{
-			const unsigned long ncycles = arg1;
-			const unsigned long num_accesses = arg2;
-			printf("Result is: %lx\n", arg1);
-			report_experiment_result(num_accesses, ncycles);
-			break;
-		}
-		case VmmCall::StartRandomAccess:
-			experimentType = MicrobenchExperiment::DCacheSize;
-			break;
-		case VmmCall::StartRandomPageAccess:
-			experimentType = MicrobenchExperiment::DTLBSize;
-			break;
-		case VmmCall::StartRandomJmp:
-			experimentType = MicrobenchExperiment::ICacheSize;
-			break;
-		case VmmCall::StartRandomPageJmp:
-			experimentType = MicrobenchExperiment::ITLBSize;
-			break;
-		case VmmCall::DoneTest:
-			experimentType = MicrobenchExperiment::Unknown;
-			break;
-		default:
-		{
-			printf("Unknown cmd: %lx %lx %lx %lx\n", cmd, arg1, arg2, arg3);
-			break;
-		}
-	}
-
+	printf("%lx %lx %lx %lx\n", cmd, arg1, arg2, arg3);
+	dump_regs(state);
 	return 0;
 }
 
 static int mini_svm_handle_exit(struct mini_svm_vmcb *vmcb, struct mini_svm_vm_state *state) {
 	__u64 exitcode = get_exitcode(&vmcb->control);
 	int should_exit = 0;
+
+	printf("Exitcode: %s\n", translate_mini_svm_exitcode_to_str((enum MINI_SVM_EXITCODE)exitcode));
 
 	// TODO: Doing this through function pointers for the respective handlers is probably better.
 	switch((enum MINI_SVM_EXITCODE)exitcode) {
@@ -403,31 +288,97 @@ void mini_svm_setup_regs(struct mini_svm_vm_regs *regs) {
 	regs->r15 = 0;
 }
 
+void dump_communication_block() {
+#define p(X) \
+	printf("%s: %x\n", #X, static_cast<unsigned>(X))
+
+	const uint64_t commBlockHva = kMiniSvmCommunicationBlockGpa + reinterpret_cast<uint64_t>(guest_memory);
+	const struct MiniSvmCommunicationBlock &block =
+		*reinterpret_cast<const struct MiniSvmCommunicationBlock *>(commBlockHva);
+
+	p(block.result);
+	p(block.operationType);
+	p(block.cipherType);
+	p(block.sourceHpa);
+	p(block.destinationHpa);
+
+#undef p
+}
+
+static int mini_svm_fd {};
+static struct mini_svm_vmcb *vmcb {};
+static struct mini_svm_vm_state *state {};
+
+template<size_t Size>
+static void setKey(struct MiniSvmCommunicationBlock &commBlock, const std::array<uint8_t, Size> &array) {
+	static_assert(Size == 16 || Size == 24 || Size == 32);
+	const uint64_t pa { gva_to_gpa( reinterpret_cast<const void *>(&array[0])) };
+	commBlock.setOperationType(MiniSvmOperation::RegisterKey);
+	commBlock.setSourceHpa(pa);
+	commBlock.setSourceSize(Size);
+
+	if (ioctl(mini_svm_fd, MINI_SVM_IOCTL_RESUME, 0) < 0) {
+		printf("Failed to ioctl mini-svm\n");
+		exit(-1);
+	}
+	if (mini_svm_handle_exit(vmcb, state)) {
+		printf("Svm exitted with a weird error\n");
+		exit(-1);
+	}
+	if (commBlock.getResult() != MiniSvmReturnResult::Ok) {
+		printf("Return result was not ok\n");
+		exit(-1);
+	}
+}
+
+static void encryptData(MiniSvmCommunicationBlock &commBlock, MiniSvmCipher cipherType, const void *input, size_t size, void *output) {
+	const uint64_t paInput { gva_to_gpa(input) };
+	const uint64_t paOutput { gva_to_gpa(output) };
+	printf("%lx %lx\n", paInput, paOutput);
+	commBlock.setOperationType(MiniSvmOperation::EncryptData);
+	commBlock.setSourceHpa(paInput);
+	commBlock.setDestinationHpa(paOutput);
+	commBlock.setSourceSize(size);
+	commBlock.setCipherType(MiniSvmCipher::AesEcb);
+
+	if (ioctl(mini_svm_fd, MINI_SVM_IOCTL_RESUME, 0) < 0) {
+		printf("Failed to ioctl mini-svm\n");
+		exit(-1);
+	}
+	if (mini_svm_handle_exit(vmcb, state)) {
+		printf("Svm exitted with a weird error\n");
+		exit(-1);
+	}
+	if (commBlock.getResult() != MiniSvmReturnResult::Ok) {
+		printf("Return result was not ok\n");
+		exit(-1);
+	}
+
+	printf("%s\n", (char *)output);
+}
+
 int main(int argc, char *argv[]) {
-	int fd = open("/dev/mini_svm", O_RDWR);
-	if (fd < 0) {
+	mini_svm_fd = open("/dev/mini_svm", O_RDWR);
+	if (mini_svm_fd < 0) {
 		printf("Failed to open mini-svm\n");
 		return -1;
 	}
 
-	struct mini_svm_vmcb *vmcb = NULL;
-	struct mini_svm_vm_state *state = NULL;
-
-	void *pages = mmap(0, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, MINI_SVM_MMAP_VM_VMCB);
+	void *pages = mmap(0, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, mini_svm_fd, MINI_SVM_MMAP_VM_VMCB);
 	if (pages == MAP_FAILED) {
 		printf("Failed to mmap vmcb\n");
 		return -1;
 	}
 	vmcb = (struct mini_svm_vmcb *)pages;
 
-	pages = mmap(0, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, MINI_SVM_MMAP_VM_STATE);
+	pages = mmap(0, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, mini_svm_fd, MINI_SVM_MMAP_VM_STATE);
 	if (pages == MAP_FAILED) {
 		printf("Failed to mmap vm state\n");
 		return -1;
 	}
 	state = (struct mini_svm_vm_state *)pages;
 
-	guest_memory = mmap(0, MINI_SVM_MAX_PHYS_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, MINI_SVM_MMAP_VM_PHYS_MEM);
+	guest_memory = mmap(0, MINI_SVM_MAX_PHYS_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, mini_svm_fd, MINI_SVM_MMAP_VM_PHYS_MEM);
 	if (guest_memory == MAP_FAILED) {
 		printf("Failed to retrieve guest memory\n");
 		return -1;
@@ -438,38 +389,12 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
-	// Try to get VM image
-	if (argc != 6) {
-		printf("Unexpected args. Expected: vm-image-file data-access-results-file page-access-results-file instruction-fetch-results-file instruction-fetch-page-results-file\n");
+	if (argc != 2) {
+		printf("Unkown args.\n");
 		return -1;
 	}
-
 	if (!load_vm_program(argv[1], guest_memory)) {
-		printf("Failed to load vm image\n");
-		return -1;
-	}
-
-	data_access_results_file = fopen(argv[2], "w+");
-	if (data_access_results_file == NULL) {
-		printf("Failed to open data access file: %s\n", argv[2]);
-		return -1;
-	}
-
-	page_access_results_file = fopen(argv[3], "w+");
-	if (page_access_results_file == NULL) {
-		printf("Failed to open page access file: %s\n", argv[3]);
-		return -1;
-	}
-
-	instruction_fetch_results_file = fopen(argv[4], "w+");
-	if (instruction_fetch_results_file == NULL) {
-		printf("Failed to open instruction fetch file: %s\n", argv[4]);
-		return -1;
-	}
-
-	instruction_fetch_page_results_file = fopen(argv[5], "w+");
-	if (instruction_fetch_page_results_file == NULL) {
-		printf("Failed to open instruction fetch page file: %s\n", argv[5]);
+		printf("Failed to laod vm\n");
 		return -1;
 	}
 
@@ -477,27 +402,50 @@ int main(int argc, char *argv[]) {
 	setup_save(&vmcb->save);
 	mini_svm_setup_regs(&state->regs);
 
-	int r = ioctl(fd, MINI_SVM_IOCTL_START, 0);
+	const uint64_t commBlockHva = kMiniSvmCommunicationBlockGpa + reinterpret_cast<uint64_t>(guest_memory);
+	struct MiniSvmCommunicationBlock &commBlock =
+		*reinterpret_cast<struct MiniSvmCommunicationBlock *>(commBlockHva);
+	bool should_exit;
+
+	// Init
+	commBlock.setOperationType(MiniSvmOperation::Init);
+	int r = ioctl(mini_svm_fd, MINI_SVM_IOCTL_START, 0);
 	if (r < 0) {
 		printf("Failed to ioctl mini-svm\n");
 		return -1;
 	}
+	should_exit = mini_svm_handle_exit(vmcb, state);
+	if (should_exit) {
+		printf("Failed to init\n");
+		exit(-1);
+	}
 
-	vmcb->control.vmcb_clean = -1;
+	// Set key
+	std::array<uint8_t, 32> key {};
+	key.fill(0x41U);
+	setKey(commBlock, key);
 
+	std::array<uint8_t, 32> data {};
+	data.fill(0x42U);
+	std::array<uint8_t, 32> output {};
+	encryptData(commBlock, MiniSvmCipher::AesEcb, data.data(), data.size(), output.data());
+
+	#if 0
 	int should_exit;
 	do {
 		should_exit = mini_svm_handle_exit(vmcb, state);
 		if (should_exit) {
 			break;
 		}
+		dump_communication_block();
 		vmcb->control.vmcb_clean = -1;
-		int r = ioctl(fd, MINI_SVM_IOCTL_RESUME, 0);
+		int r = ioctl(mini_svm_fd, MINI_SVM_IOCTL_RESUME, 0);
 		if (r < 0) {
 			printf("Failed to ioctl mini-svm\n");
 			return -1;
 		}
 	} while(1);
+	#endif
 
 	return 0;
 }
