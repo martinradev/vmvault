10151730d622d560829d3a6c6863ae2e27466277
Martin Radev <martin.b.radev@gmail.com>
2021-08-21 18:00:38 +0300

    Fix build script

--- build.sh
+++ build.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-KERNEL_DIR_NESTED=/home/sisu/code/linux-kernels/linux/
-KERNEL_DIR_HOST=/usr/lib/modules/5.13.0-rc4+/build
-
 if [ "$1" = "build_nested" ]; then
-	KERNEL_DIR=$KERNEL_DIR_NESTED make all
+	KERNEL_DIR=/home/sisu/code/linux-kernels/linux/ make all
+elif [ "$1" = "build" ]; then
+	KERNEL_DIR=/home/sisu/code/linux-kernels/linux/ make all
 elif [ "$1" = "run_nested" ]; then
 	cd ./testing
 	./run.sh
-elif [ "$1" = "build" ]; then
-	KERNEL_DIR=$KERNEL_DIR_HOST make all
 elif [ "$1" = "run" ]; then
-	sudo insmod ./vmvault-mini.ko vmvault_debug_enable_logging=0
+	sudo insmod ./vmvault.ko vmvault_debug_enable_logging=0
 else
 	echo "Unrecognized command " "$1"
 fi
